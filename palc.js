d4ed2735bda53e02b525cb9355544de65134da22
kw3st00 <154664921+kw3st00@users.noreply.github.com>
2023-12-29 00:34:23 +0100

    Add files via upload

---
+++ palc.js
@@ -0,0 +1,12 @@
+function mybutton() {
+    alert('This is the pricetag for this product')
+}
+
+function change() {
+    let xy = document.getElementById ("know") // div (text)
+    xy.textContent = ("Unlock the full potential of your gadgets with our exclusive Learn More section. Elevate your tech knowledge, explore tips, and discover hidden features. Stay ahead in the digital world!")
+
+
+    xy.style.backgroundColor = "gold"
+    xy.style.color = "black"
+}
